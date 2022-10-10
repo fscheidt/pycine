d05225ade43beb4a45771b25529a0fc33a2141a4
Felippe <felippescheidt@gmail.com>
2022-10-10 20:41:50 -0300

    up

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from fastapi import FastApi
+from fastapi import FastAPI
 
-app = FastApi()
+app = FastAPI()
 # fornecido um id, retorno o 
 # json do filme
 # /movie/1
 @app.get("/movie")
 async def get_movie():
     return {"title": "Avatar"}
+
+# uvicorn main:app --reload
 
 # pip install -r requirements.txt
 # source env/
