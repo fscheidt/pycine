941a06030206646e5ea95e49ce45cc9cca4cc9d2
Felippe Scheidt <felippescheidt@gmail.com>
2022-10-10 21:51:22 -0300

    get find

--- main.py
+++ main.py
@@ -4,9 +4,33 @@ app = FastAPI()
 # fornecido um id, retorno o 
 # json do filme
 # /movie/1
-@app.get("/movie")
-async def get_movie():
-    return {"title": "Avatar"}
+@app.get("/movie/{id}")
+async def get_movie(id: int):
+    import json
+    # lista de dictionary
+    data = json.load(open('filmes.json'))
+    for filme in data:
+        if filme['id'] == id:
+            return filme
+    return {}
+    # return {"id": id, "title": "Avatar"}
+
+@app.get("/find/{title}")
+async def find(title: str):
+    import json
+    data = json.load(open('filmes.json'))
+    encontrou = []
+    for filme in data:
+        # in - contains (ou contem um substring)
+        if title.lower() in filme['title'].lower():
+            # append - adiciona na lista
+            encontrou.append(filme)
+    return encontrou
+
+@app.get("/")  # HTTP GET
+async def home():
+    return {"msg": "Hello"}
+
 
 # uvicorn main:app --reload
 
