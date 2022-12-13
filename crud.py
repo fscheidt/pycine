783d1bf46a1ed52f17f6f2145aef5e916ebb8edb
Felippe <felippescheidt@gmail.com>
2022-12-12 22:07:20 -0300

    fix user name not save

--- crud.py
+++ crud.py
@@ -13,7 +13,7 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(email=user.email, password=user.password)
+    db_user = models.User(name=user.name, email=user.email, password=user.password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
